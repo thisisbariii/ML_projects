d29b31aad4664720c5d4fad342ef3bb919730b93
thisisbariii <skbad911@gmail.com>
2026-08-26 16:39:53 +0530

    parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -54,13 +54,46 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            params ={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                },
+                "XGB Regressor":{
+                    'learning_rate':[.1,0.1,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256,]
+                }
+            }
             # Evaluate all models
             model_report = evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                param=params
+
             )
 
             print("Model Report:", model_report)
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,28 +19,31 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
 
-        for model_name, model in models.items():
+        for i in range(len(list(models))):
 
-            # Train the model
-            model.fit(X_train, y_train)
+          model_name = list(models.keys())[i]
 
-            # Prediction on training data
-            y_train_pred = model.predict(X_train)
+          model = list(models.values())[i]
 
-            # Prediction on testing data
-            y_test_pred = model.predict(X_test)
+          model_param = param[model_name]
 
-            # Calculate scores
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+          gs = GridSearchCV(model, model_param, cv=3)
+          gs.fit(X_train, y_train)
 
-            # Save test score
-            report[model_name] = test_model_score
+          model.set_params(**gs.best_params_)
+          model.fit(X_train, y_train)
 
+          y_train_pred = model.predict(X_train)
+          y_test_pred = model.predict(X_test)
+
+          train_model_score = r2_score(y_train, y_train_pred)
+          test_model_score = r2_score(y_test, y_test_pred)
+
+        report[model_name] = test_model_score
         return report
 
     except Exception as e:
