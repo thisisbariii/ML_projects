d62322825cd32647fdbe665b8e4d8ab0a29ddba9
thisisbariii <skbad911@gmail.com>
2026-08-27 16:20:21 +0530

    Fix Flask port configuration

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-
+import os
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
@@ -27,8 +27,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
 
         )
          pred_df= data.get_data_as_data_frame() 
@@ -38,5 +38,6 @@ def predict_datapoint():
          results=predict_pipline.predict(pred_df)
          return render_template('home.html',results=results[0])
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
